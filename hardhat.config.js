b5ba9a9dbcc56f6be1d6462f4a4cc81e5f13a461
Ethan Wessel <ejwessel@gmail.com>
2022-02-15 10:10:16 -0700

    added flattener

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,37 @@
+require("dotenv").config();
+
 require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-web3');
+require("@nomiclabs/hardhat-solpp");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+function getNetworkUrl(networkName) {
+  const alchemyKey = process.env.ALCHEMY_API_KEY || "";
+  const url = `https://eth-${networkName}.alchemyapi.io/v2/` + alchemyKey;
+  return url;
+}
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  networks: {
+    // mainnet: {
+    //   url: getNetworkUrl('mainnet'),
+    //   accounts: [process.env.PRIVATE_KEY],
+    //   baseFeePerGas: 140e9,
+    //   timeout: 1000000,
+    // },
+  },
+  solidity: {
+    version: "0.8.0",
+    optimizer: {
+      enabled: true,
+      runs: 999999,
+    },
+  },
+  mocha: {
+    timeout: 1000000,
+  },
+  solpp: {
+    "cwd": "./contracts",
+    "noFlatten": false
+  }
+}
