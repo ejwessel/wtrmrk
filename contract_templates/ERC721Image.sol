63fbc0047738bddf060c7d3c142457051e60386a
Ethan Wessel <ejwessel@gmail.com>
2022-02-17 11:33:53 -0700

    remove comments

--- contract_templates/ERC721Image.sol
+++ contract_templates/ERC721Image.sol
@@ -1,5 +1,3 @@
-// SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -7,25 +5,24 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-// File: contracts/APYNFTs.sol
 
 /**
- * @title APYNFTs contract
+ * @title ERC721Image Contract
  * @dev Extends ERC721 Non-Fungible Token Standard basic implementation
  */
 contract ERC721Image is ERC721, Ownable {
     using Address for address;
     using Address for address payable;
 
-    uint256 constant public fee = {{ fee }};
-    bytes32 constant public root = {{ root }};
-    string constant public sig = "{{ sig }}";
+    uint256 public constant fee = {{ fee }};
+    bytes32 public constant root = {{ root }};
+    string public constant sig = "{{ sig }}";
     string public baseURI;
 
     constructor(
         bytes32[] memory proof,
         string memory uri
-    ) ERC721({{ name }}, {{ symbol }}) payable {
+    ) ERC721("{{ name }}", "{{ symbol }}") payable {
         if(!verify(proof, msg.sender)) {
             require(msg.value == fee, 'address not whitelsited, fee required for deployment');
             payable({{ owner }}).transfer(msg.value);
@@ -37,10 +34,6 @@ contract ERC721Image is ERC721, Ownable {
 
     function _baseURI() internal view override returns (string memory) {
         return baseURI;
-    }
-
-    function setBaseURI(string memory newBaseURI) public onlyOwner {
-        baseURI = newBaseURI;
     }
 
     function verify(bytes32[] memory proof, address to)
